a0e9b420617be0ca43e9c3efc2608aa3d1e3f05b
VenushkaD <vdhambarage@gmail.com>
2021-09-18 16:13:58 +0530

    Registration Username Validation

--- Login 2/Registration.cs
+++ Login 2/Registration.cs
@@ -29,57 +29,54 @@ namespace Login_2
             MySqlConnection con = new MySqlConnection("server=localhost;uid=root;pwd=;database=lbms;SSL Mode=none;");
             string query = "INSERT INTO user VALUES('" + txtFirstName.Text + "','" + txtLastName.Text + "','" + txtUsername.Text + "','" + txtEmail.Text + "','" + txtPassword.Text + "')";
             MySqlCommand cmd = new MySqlCommand(query, con);
-
-            
-
-
-            if (txtFirstName.Text == "" || txtLastName.Text == "" || txtUsername.Text == "")
+            string reg = "SELECT COUNT(*) FROM user WHERE Username = '" + txtUsername.Text + "'";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(reg, con);
+            DataTable dt = new DataTable();
+            try
             {
-                MessageBox.Show("Fields are Empty!");
-            }
-            else if (txtPassword.Text == "" || txtConfirmPassword.Text == "")
-            {
-                MessageBox.Show("Password Fields are empty!");
-            }
-            else if (txtPassword.Text == txtConfirmPassword.Text)
-            {
-                try
+                adapter.Fill(dt);
+                if (txtFirstName.Text == "" || txtLastName.Text == "" || txtUsername.Text == "" || txtPassword.Text == "" || txtConfirmPassword.Text == "")
+                {
+                    MessageBox.Show("Fields are Empty!");
+                }
+                else if (txtPassword.Text != txtConfirmPassword.Text)
+                {
+                    MessageBox.Show("Password Does Not Match", "REGISTRATION FAILED!");
+                    txtPassword.Text = "";
+                    txtConfirmPassword.Text = "";
+                    txtPassword.Focus();
+                }
+                else if (int.Parse(dt.Rows[0][0].ToString()) >= 1)
+                {
+                    MessageBox.Show("This Username is Already Registered!", "REGISTRATION FAILED!");
+                    txtUsername.Text = "";
+                    txtUsername.Focus();
+                }
+                else if (txtPassword.Text == txtConfirmPassword.Text)
                 {
                     con.Open();
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("You Have Registered Successfully", "REGISTRATION SUCCESS!");
 
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.ToString());
-                }
-                finally
-                {
-                    con.Close();
-                }
+                }    
+            }            
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
             }
+            finally
+            {
+                con.Close();
+            }
+
             
-            else if(txtPassword.Text != txtConfirmPassword.Text)
-            {
-                MessageBox.Show("Password Does Not Match", "REGISTRATION FAILED!");
-                txtPassword.Text = "";
-                txtConfirmPassword.Text = "";
-                txtPassword.Focus();
-            }
+            
+            
 
-            string reg = "SELECT COUNT(*) FROM user WHERE Username = '" + txtUsername.Text + "'";
-            MySqlDataAdapter adapter = new MySqlDataAdapter(reg, con);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
+            
 
 
-            if (int.Parse(dt.Rows[0][0].ToString()) >= 1)
-            {
-                MessageBox.Show("This Username is Already Registered!", "REGISTRATION FAILED!");
-                txtUsername.Text = "";
-                txtUsername.Focus();
-            }
+            
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
